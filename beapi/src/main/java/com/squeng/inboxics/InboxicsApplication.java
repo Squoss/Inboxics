748daee3c275c1814d1cd582ecdbdffb8d152b48
Squeng <paul@squeng.com>
2024-02-09 22:16:51 +0100

    added basic Spring Boot project for the backend

---
+++ beapi/src/main/java/com/squeng/inboxics/InboxicsApplication.java
@@ -0,0 +1,13 @@
+package com.squeng.inboxics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InboxicsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InboxicsApplication.class, args);
+	}
+
+}
---
+++ beapi/src/test/java/com/squeng/inboxics/InboxicsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.squeng.inboxics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InboxicsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
